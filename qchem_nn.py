81c20c681d97115c06a6f1aaafc61ddc1a6efd71
John Alberse <john.r.alberse-1@ou.edu>
2017-04-19 17:00:12 -0500

    Changed output to QM. Addes code to create validation dataset. Implemented simple model, which needs testing

--- qchem_nn.py
+++ qchem_nn.py
@@ -9,73 +9,71 @@ import numpy as np
 import tensorflow as tf
 import nn_input_handler as nni
 
-#size of the input
-X_SIZE = nni.COULOMB_DIMENSION * nni.COULOMB_DIMENSION
+#size of the input 
+#TODO: Keep updated
+X_SIZE = 2645
 
 def read_and_decode_single_example(filename):
     """
     Decodes a single example from a .tfrecords file for training
     """
-    #filename queue, splits up data and keeps size down
+    #creates a filename queue, splits up data and keeps size down
     filename_queue = tf.train.string_input_producer([filename], 
                                                     num_epochs=None)
     reader = tf.TFRecordReader()
     #note that _ refers to the previous symbol
     #reader converts back into actual example
+    #will go to the next file in the queue if necessary
     _, serialized_example = reader.read(filename_queue)
     features = tf.parse_single_example(
             serialized_example,
             #here we tell it what features to pull
             features={
-                    'bc': tf.FixedLenFeature([2654], tf.float32),
+                    'bc': tf.FixedLenFeature([X_SIZE], tf.float32),
                     'e': tf.FixedLenFeature([], tf.float32)
             })
     bc = features['bc']
     e = features['e']
-    
+    #input == bc, label == e
     return bc, e
 
-#TODO: I definitely need a thread coordinator somewhere
+#TODO: Assemble a larger database and test this
 def simple_model():
-    """
-    A simple model using high level tf functions. only input and output layers
-    """
-    #through some backend magic, batches can keep using this
-    bc, e = read_and_decode_single_example("ges.tfrecords")
+    try:
+        # gets examples
+        bc, e = read_and_decode_single_example("training.tfrecords")
+        # create batches
+        #TODO: Tweak values
+        bcs_batch, es_batch = tf.train.shuffle_batch(
+                [bc, e], 
+                batch_size = 25,
+                capacity=1000,
+                min_after_dequeue=500)
+        
+        #construct the model here
+        w = tf.get_variable('w1', [X_SIZE, 1])
+        y_pred = tf.matmul(bcs_batch, w)
+        loss = tf.nn.sparse_softmax_cross_entropy_with_logits(y_pred, es_batch)
+        
+        #monitoring
+        loss_mean = tf.reduce_mean(loss)
+        
+        #use predefined training algorithm
+        train_op = tf.rain.AdamOptimizer().minimize(loss)
+        
+        #tell TensorFlow to set itself up to evaluate the model
+        sess = tf.Session()
+        init = tf.initialize_all_variables()
+        sess.run(init)
+        tf.train.start_queue_runners(sess=sess)
+        
+        while True:
+            _, loss_val = sess.run([train_op, loss_mean])
+            print (loss_val)
+        
+    except FileNotFoundError:
+        print('training.tfrecords not found. Please assemble dataset using'
+              ' input handler')
     
-    #creates places for input
-    x = tf.placeholder(tf.float32, [None, X_SIZE])
+
     
-    #currently not deep, just input and output layers
-    # [input size, output size]
-    W = tf.Variable(tf.zeros([X_SIZE, 1]))
-    #sze of 1, to add to output
-    b = tf.Variable(tf.zeros([1]))
-    
-    #define the model
-    y = tf.nn.softmax(tf.matmul(x, W) + b)
-    #cross entropy prep
-    y_ = tf.placeholder(tf.float32, [None, 1])
-    
-    #calculate cross entropy
-    cross_entropy = cross_entropy = tf.reduce_mean(
-                    tf.nn.softmax_cross_entropy_with_logits(labels=y_, 
-                                                            logits=y))
-    #define one step of training procedure
-    train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-    
-    #create batches
-    #TODO tweak values
-    bcs_batch, es_batch = tf.train.shuffle_batch(
-            [bc, e], 
-            batch_size=25,
-            capacity=2000,
-            min_after_dequeue=1000)
-    
-    #TODO: Figure out how to merge input flow like this with project
-    
-    sess = tf.Session()
-    init = tf.initialize_all_variables()
-    sess.run(init)
-    tf.train.start_queue_runners(sess=sess)
-    #es, bcs = sess.run([es_batch, bcs_batch])
