d07ed2aa2b16f687eadc3ff055c752a742c7bd8d
Yihan Shao <yihan@schooner2.oscer.ou.edu>
2017-05-11 15:45:28 -0500

    python script to generate QM energy values

--- run_qchem_calculations.py
+++ run_qchem_calculations.py
@@ -7,22 +7,22 @@ from qm_analysis import *
 subset = int(sys.argv[1])
 energyfile = 'energies.batch'+str(subset)+".dat"
 
-options  = {'method':'b3lyp', 'dft_d':'empirical_grimme3', 'basis':'6-31G*'}
+options  = {'method':'b3lyp', 'dft_d':'empirical_grimme3', 'basis':'6-31G*', 'sym_ignore':'true'}
 rem = QChemRem(options)
 
-count = 0
 previous_energies = {}  # energies already computed
 if os.path.isfile(energyfile):
 	datf = open(energyfile, "r")
 	for line in datf.readlines():
 		cols = line.split()
 		previous_energies[cols[0]] = float(cols[1])
-	print("previous_energies:", previous_energies)
+	#print("previous_energies:", previous_energies)
 	enf = open(energyfile, "a")
 else:
 	enf = open(energyfile, "w")
 
-for xyzfile in glob.glob("data/gdb_subset/training/"+"mol"+str(subset)+"*xyz"):
+count = 0
+for xyzfile in glob.glob("training/"+"mol"+str(subset)+"*xyz"):
 	pos1  = xyzfile.find("mol")+3
 	index = int(xyzfile[pos1:-4])
 	if str(index) in previous_energies.keys():
@@ -30,16 +30,18 @@ for xyzfile in glob.glob("data/gdb_subset/training/"+"mol"+str(subset)+"*xyz"):
 		continue
 
 	molecule = read_xyz(xyzfile)
-	print("xyzfile: ", index, xyzfile, index, molecule.natoms)
-	qchem_input_generator("qchem.inp", molecule, rem)	
-	#os.system("qchem -nt 20 qchem.inp qchem.out")
-	os.system("qchem qchem.inp qchem.out")
+	#print("xyzfile: ", index, xyzfile, index, molecule.natoms)
+	inpfile = "qchem"+str(subset)+".inp"
+	outfile = "qchem"+str(subset)+".out"
+	qchem_input_generator(inpfile, molecule, rem)	
+	os.system("qchem -nt 20 "+inpfile+" "+outfile)
+	#os.system("qchem qchem.inp qchem.out")
 
-	qcout = QChemOut("qchem.out")
+	qcout = QChemOut(outfile)
 	energy = qcout.scf_energy
 	enf.write("%10d %20.10f \n" % (index, energy))
-
 	count += 1
-	if count > 4: break
+	if count > 800: break
+
 	
 
