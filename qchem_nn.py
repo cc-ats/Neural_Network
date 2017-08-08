607d5a1892b2dc0b79ba8aa1d4fdd412b885db48
jalberse <john.r.alberse-1@ou.edu>
2017-08-07 20:35:06 -0500

    Full implementation of early stopping including calculation of percent error on validation set. Trained network down to a ~2% error over validation set.

--- qchem_nn.py
+++ qchem_nn.py
@@ -7,7 +7,6 @@ Created on Thu Apr  6 14:38:58 2017
 """
 import tensorflow as tf
 import matplotlib.pyplot as plt
-import math
 
 #TODO: Keep updated
 #Size of input
@@ -15,7 +14,7 @@ INPUT_SIZE = 2645
 
 #parameters
 learning_rate = .0001
-epochs = 20
+n_epochs = 500
 batch_size = 25
 display_step = 1
 #total number of mols in training set. Keep updated if change data.
@@ -48,7 +47,7 @@ biases = {
         'rescale_bias' : tf.Variable(initial_value=-301.)
 }
 
-#TODO: A function which lets us make predictions with the model (use it)
+#TODO: Function to load checkpoint and evaluate new molecules
 
 def model(x, weights, biases):
     """
@@ -85,6 +84,12 @@ def train_model():
                 batch_size = batch_size,
                 capacity = 1000, 
                 min_after_dequeue = 500)
+        val_bc, val_e = read_and_decode_single_example('validation.tfrecords')
+        val_bcs_batch, val_es_batch = tf.train.shuffle_batch(
+                [val_bc, val_e], 
+                batch_size = batch_size,
+                capacity = 500, 
+                min_after_dequeue = 200)
         
         #construct training model
         training_model = model(x, weights, biases)
@@ -94,23 +99,20 @@ def train_model():
         cost = tf.reduce_mean(tf.squared_difference(es_batch, training_model))
         optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
         
-        #number of epochs to train on
-        n_epochs = 3000
-        
         saver = tf.train.Saver()
         
-        #TODO: Add early stopping once validation tested
         #TODO: Add dropout
+        #TODO: Add other strategies for improvement after dropout
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
             coord = tf.train.Coordinator()
             threads = tf.train.start_queue_runners(sess=sess, coord=coord)
-            is_improving = True
+            improving = True
+            accuracy_over_time = []
             for i in range(n_epochs):
-                if (is_improving == True):
+                if (improving == True):
                     # Each pass through this loop is ONE epoch
                     epoch_loss = 0
-                    accuracy_over_time = []
                     #Within for loop is 1 epoch
                     for j in range(int(n_training_items/batch_size)):
                         #Each pass through this loop is one batch
@@ -123,12 +125,28 @@ def train_model():
                     #prints epoch loss after each epoch
                     print('Epoch', i+1, 'completed out of', n_epochs,
                           'epoch loss:', epoch_loss)
+                
                     
-                    #Obtain and append accuracy over whole validation set
-                    accuracy_over_time.append(compute_accuracy(sess, 
-                                                     'validation.tfrecords'))
+                    #Calculate accuracy
+                    #DO NOT put in own function for abstraction; breaks threads
+                    total_percent_error = 0.
+                    for _ in range (int(n_test_and_valid_items / batch_size)):
+                        e_estimates = sess.run(model(x, weights, biases), 
+                                           feed_dict = {x: bcs_batch.eval()})
+                        pe = tf.reduce_mean(tf.abs(tf.div(
+                                tf.subtract(es_batch, e_estimates), es_batch)))
+                        #total percent error over batch
+                        total_percent_error += pe * 100
+                    #Avg percent error for batch
+                    curr_acc = (total_percent_error / (
+                            int(n_test_and_valid_items / batch_size))).eval()
+                    
+                    #Display and record accuracy for this epoch
+                    print('Current Percent Error: ', curr_acc)
+                    accuracy_over_time.append(curr_acc)
+                    
                     #early stopping implementation
-                    is_improving = is_improving(10, accuracy_over_time)
+                    improving = is_improving(10, accuracy_over_time, i)
                     
             #shows graph of accuracy over time on validation set
             plt.plot(accuracy_over_time)
@@ -141,60 +159,26 @@ def train_model():
             
             coord.request_stop()
             coord.join(threads)
+            sess.close()
             
         
     except FileNotFoundError:
         print('file note found error')
 
-def is_improving(patience, accuracy_over_time, epoch_num):
+def is_improving(patience, accuracy_over_time, epoch):
     """
     Checks if the network has improved in most recent window
     of length patience
     
-    patience: Number of most recent accuracies to check
+    patience: Number of most recent accuracies to check.
     accuracy_over_time: list of all accuracies
-    epoch_num: number of epochs completed
+    epoch: number of epochs completed, from 0
     """
-    if ( epoch_num > patience):
-        first_acc = accuracy_over_time[-patience]
-        for j in range (patience, 0, -1):
-            if (accuracy_over_time[-j] > first_acc):
-                #there is improvement, continue training
-                break
-            elif (j == 1):
-                #No accuracy was higher than the first
-                #network has stopped improving, stop training
-                return False
-        #is improving in patience window
-        return True
-    #Hasn't reached patience threshold yet
+    if (epoch > patience):
+        past_epoch = epoch - patience
+        if (accuracy_over_time[past_epoch] < accuracy_over_time[epoch]):
+            return False
     return True
-
-#TODO: Implement accuracy computations in batches. Unnacceptably slow ATM.
-def compute_accuracy(sess, filename):
-    """
-    This should compute and return accuracy (in form of avg % error over batch
-    of model over the entirity of the data in a tfrecord file
-    
-    Used ONLY for testing and validation sets, which must be of the same length
-    
-    args:
-        sess: the session the model is trained in
-        filename: the tfrecord to calculate accuracy over
-    """
-    #loop over each example
-    total_percent_error = 0.0
-    for _ in range (n_test_and_valid_items):
-        #obtain real geometry and energy
-        #queue should make this just grab the next example
-        bc, e_real = read_and_decode_single_example(filename)
-        #run the model with this geometry, find estimated energy
-        #note bc is in a list to increase its shape by 1
-        e_estimate = sess.run(model([bc], weights, biases))
-        #Compare the two find percent error add percent error to total for avg
-        total_percent_error += math.fabs((e_estimate - e_real) / e_real)
-    #return average percent error
-    return total_percent_error / n_test_and_valid_items
     
 
 def read_and_decode_single_example(filename):
@@ -202,6 +186,7 @@ def read_and_decode_single_example(filename):
     Decodes a single example from a .tfrecords file for training
     """
     #creates a filename queue, splits up data and keeps size down
+    #right now only using one file as standard, but good for future
     filename_queue = tf.train.string_input_producer([filename], 
                                                     num_epochs=None)
     reader = tf.TFRecordReader()
